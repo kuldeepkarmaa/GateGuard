715e566912d5a4b076d21ca58bce03b9df47c715
kuldeepkarmaa <kuldeepkarma816@gmail.com>
2026-08-04 11:35:12 +0530

    Update Resident.js

--- backend/models/Resident.js
+++ backend/models/Resident.js
@@ -4,8 +4,7 @@ const residentSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   flatNumber: { type: String, required: true },
   block: { type: String, required: true },
-  vehicleNumber: { type: String },
-  familyMembers: [{ name: String, relation: String, phone: String }]
+  vehicleNumber: { type: String }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Resident', residentSchema);
