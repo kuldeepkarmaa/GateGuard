18f5d29534815ee8512fc1d17cae23925648db85
kuldeepkarmaa <kuldeepkarma816@gmail.com>
2026-08-04 11:35:16 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider, AuthContext } from './context/AuthContext';
 
